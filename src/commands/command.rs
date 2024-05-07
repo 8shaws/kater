3d7a7a23b4abab810245f7d2bf68d1e0f85f8fe2
Akash Shaw <shawakash2003@gmail.com>
2024-05-07 18:33:58 +0530

    chore: adds command module

---
+++ src/commands/command.rs
@@ -0,0 +1,37 @@
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+pub enum Command {
+    #[command(description = "Get to know what we provide you with😊")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+}
+
+pub async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Username(username) => {
+            bot.send_message(msg.chat.id, format!("Your username is @{username}."))
+                .await?
+        }
+        Command::UsernameAndAge { username, age } => {
+            bot.send_message(
+                msg.chat.id,
+                format!("Your username is @{username} and age is {age}."),
+            )
+            .await?
+        }
+    };
+
+    Ok(())
+}
